892a1be86e450b48579789216fd91813c12c168e
xGIANOx <studioenricogian@gmail.com>
2023-05-04 16:05:05 +0200

    [add] table to display hotels

--- index.php
+++ index.php
@@ -41,11 +41,63 @@ $hotels = [
 
 ];
 
-foreach ($hotels as $hotel) {
-    foreach ($hotel as $key => $value) {
-        var_dump($key, $value);
 
-    }
-}
+
+// foreach ($hotels as $hotel) {
+//     foreach ($hotel as $key => $value) {
+//         var_dump($key, $value);
+
+//     }
+// }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+<table class="table table-success table-striped mt-5">
+  <thead>
+    <tr>
+      <th scope="col">NAME</th>
+      <th scope="col">DESCRIPTION</th>
+      <th scope="col">PARKING</th>
+      <th scope="col">VOTE</th>
+      <th scope="col">DISTANCE TO CENTER</th>
+
+
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($hotels as $hotel) : ?>
+          
+        <tr>
+          <?php foreach ($hotel as $key => $value) : ?>
+            <?php if ($key === 'parking') : ?>
+                <td>
+                    <?php echo $value ? 'Yes' : 'No'; ?>
+                </td>
+            <?php else: ?>
+                <td>
+                    <?= "$value" ?>
+                </td>
+            <?php endif; ?>
+          <?php endforeach; ?>
+        </tr>
+  <?php endforeach; ?>
+</tbody>
+</table>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
